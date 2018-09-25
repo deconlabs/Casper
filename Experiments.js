30bbb1bccf1bbb437a2c262e3c3a54ecc18964c8
casper-paper <43361072+casper-paper@users.noreply.github.com>
2018-09-25 14:27:10 +0800

    Add files via upload

--- Experiments.js
+++ Experiments.js
@@ -1,18 +1,3 @@
-makeTextFile = function (text) {
-    var data = new Blob([text], {type: 'text/plain'});
-
-    // If we are replacing a previously generated file we need to
-    // manually revoke the object URL to avoid memory leaks.
-    if (textFile !== null) {
-      window.URL.revokeObjectURL(textFile);
-    }
-
-    var textFile = window.URL.createObjectURL(data);
-
-    // returns a URL you can use as a href
-    return textFile;
-};
-
 doExperimentFigure3a = function() {
     var tau = 2000;
     var traceEpochStep = 100;
